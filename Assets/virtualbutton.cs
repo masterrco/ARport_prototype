108d1af8f1b7ad1ccfe26810a867f239331e8d98
masterrco <masterrco@yahoo.com>
2017-04-23 02:13:07 -0700

    initial commit

---
+++ Assets/virtualbutton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class virtualbutton : MonoBehaviour, IVirtualButtonEventHandler {
+
+    private GameObject contextButton;
+    private GameObject buttonParent;
+    // Use this for initialization
+    void Start () {
+        contextButton = GameObject.Find("contextButton");
+        buttonParent = GameObject.Find("Notification");
+        contextButton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnButtonPressed(VirtualButtonAbstractBehaviour vb)
+    {
+        buttonParent.active = false;
+    }
+
+    public void OnButtonReleased(VirtualButtonAbstractBehaviour vb)
+    {
+        buttonParent.active = false;
+    }
+}
